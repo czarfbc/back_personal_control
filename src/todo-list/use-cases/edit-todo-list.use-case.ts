8754a6acbde261855e20f4dc86ef0c5ec25fefa7
cezarfbc <cezarfbc@gmail.com>
2024-06-23 23:12:14 -0300

    fix todolist logic with userid and update todo

---
+++ src/todo-list/use-cases/edit-todo-list.use-case.ts
@@ -0,0 +1,13 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { ITodoListRepository } from '../repository/todo-list.repository';
+import { EditTodoListInput } from '../dto/edit-todo-list.input';
+
+@Injectable()
+export class EditTodoListUseCase {
+  @Inject('ITodoListRepository')
+  private todoListRepository: ITodoListRepository;
+
+  async execute(input: EditTodoListInput) {
+    return await this.todoListRepository.update(input);
+  }
+}
